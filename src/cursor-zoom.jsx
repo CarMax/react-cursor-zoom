83533a8e7be0a8e0b6827aa15ad51b6b4c801d60
Jared Lotti <jaredlotti@gmail.com>
2016-10-21 16:14:31 -0400

    require bug

--- src/cursor-zoom.jsx
+++ src/cursor-zoom.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Magnifier from './magnifier.jsx';
+import Magnifier from './magnifier';
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
